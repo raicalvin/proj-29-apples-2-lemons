b9c8c6d2281d92190d36cb84c59252c4e402ce20
Calvin Rai <raicalvin@gmail.com>
2018-05-16 08:29:10 -0700

    Updated modified files

--- app/src/main/java/com/a29moon/apples2lemons/MainActivity.java
+++ app/src/main/java/com/a29moon/apples2lemons/MainActivity.java
@@ -3,7 +3,9 @@ package com.a29moon.apples2lemons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,17 +15,33 @@ public class MainActivity extends AppCompatActivity {
     double item1Quantity;
     double item1Result;
 
+    String spinnerSelectionUnit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.units_drop_down_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.units_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
+
 
     public void calculateUnitCosts(View view) {
 
         // Connect the Views to the Java code:
         EditText edtxt_item1_price = findViewById(R.id.edtxt_item1_price);;
         EditText edtxt_item1_quantity = findViewById(R.id.edtxt_item1_quantity);
+
 
         item1Price = Double.parseDouble(edtxt_item1_price.getText().toString());
         item1Quantity = Double.parseDouble(edtxt_item1_quantity.getText().toString());
@@ -36,13 +54,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateResults() {
         TextView txtview_item1_result = findViewById(R.id.txtview_item1_result);
-
         txtview_item1_result.setText(formatResult(item1Result));
 
     }
 
     public String formatResult(double numToFormat) {
         String formattedResult = "$" + String.format("%.2f", numToFormat);
+
+        // This is where the text string will determine which unit to display
+
         return formattedResult;
     }
 

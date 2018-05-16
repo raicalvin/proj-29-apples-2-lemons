86670f9a1686ff3b899ccae5cbe0c66056a53e46
Calvin Rai <raicalvin@gmail.com>
2018-05-16 12:02:19 -0700

    Added a second XML layout to hold default "item cards" to be added later via button onClick.

--- app/src/main/java/com/a29moon/apples2lemons/MainActivity.java
+++ app/src/main/java/com/a29moon/apples2lemons/MainActivity.java
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void addAdditionalItemLayout(View v) {
+
+    }
+
     /*
 
     public void calculateUnitCosts(View view) {
